e251bd43c43e6deb68b366defe04b0d361e10102
Erika Oliveira <139921490+erikaoliveiracorrea@users.noreply.github.com>
2025-09-19 00:02:05 +0000

    implementando jogo da velha

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+    //estru8tura dos dados
+    int l, c;
+    char jogo[3][3];
+
+    //inicializar estrutura de dados
+    for(l = 0; l < 3; l++){
+        for(c = 0; c < 3; c++){
+            jogo[l][c] = ' ';
+        }
+    }
+    //imprimir jogo
+    printf(" 0   1   2\n");
+    for (l = 0; l < 3; l++){
+        for ( c = 0; c < 3; c++){
+            printf(" %c ", jogo[l][c]);
+            if (c < 2){
+                printf("|");
+            }
+            if (c == 2){
+                printf(" %d  ", l);
+            }
+            
+        }
+        if (l < 2){
+            printf("\n-----------");
+            printf("\n");
+        }
+    }
+    
+
+    return 0;
+}
